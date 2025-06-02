6213c87e76fad43ca5db916eda7407b3c00b59bb
Teodora Ivanova <teodoraivanova007@abv.bg>
2025-06-02 17:03:06 +0300

    markup actions cleanup

---
+++ src/controllers/accessoryController.js
@@ -0,0 +1,14 @@
+// const router = require('express').Router();
+// const accessoryController = require('../services/accessoryController');
+
+// router.get('/create', (req, res) => {
+// res.render('accessory/create');
+// });
+
+// router.post('/create', async(req, res) => {
+// const {name, descpription, imageUrl} = req.body;
+// await accessoryService.create({name, descpription, imageUrl});
+// res.redirect('/');
+// });
+
+// module.exports = router;
